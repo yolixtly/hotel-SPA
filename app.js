935e4f169d1379cb7181fffac4c5769be5649c3e
yolixtly <yolixtly_13@hotmail.com>
2016-10-11 11:52:25 -0400

    basic routing

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 //if you have or not dependencies specify it within the array.
 // this is part of the module setter syntax
-angular.module('myApp', []);
+angular.module('myApp', ['ngRoute'])
+	.config(config)
+
+function config ($routeProvider){
+		$routeProvider.when('/', {
+			template: '<h1>this is the main page Yolix!!</h1>'
+		}).when('/about', {
+			template: '<h1> this is the about page </h1>'
+		})
+}
---
+++ controller.js
@@ -0,0 +1,6 @@
+angular.module('myApp').controller('MyController', MyController)
+
+function MyController(){
+	var vm = this;
+	vm.name = "Yolix";
+};
